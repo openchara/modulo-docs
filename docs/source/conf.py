0b96087b9c87cda92429c8346e1da65ca3c7bd2c
Arthur Beck <averse.abfun@gmail.com>
2023-04-10 16:31:30 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'CharaGame'
+copyright = '2023, OpenChara'
+author = 'OpenChara'
 
 release = '0.1'
 version = '0.1.0'
